407c09b133f5b85d1cf19ddce9e458c0f534c984
Marco Terres <Marco Terres@marco>
2016-10-01 12:26:52 -0300

    Entidade Sexo

--- MavenProject1/src/main/java/br/com/project1/model/entities/Pessoa.java
+++ MavenProject1/src/main/java/br/com/project1/model/entities/Pessoa.java
@@ -7,7 +7,9 @@ import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
+import org.hibernate.annotations.ForeignKey;
 
 @Entity 
 @Table (name="pessoa")
@@ -34,6 +36,10 @@ public class Pessoa implements Serializable {
     @Column(name="DataDoCadastro", nullable=false)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataCadastro;
+    
+    @ManyToOne(optional=false)
+    @ForeignKey(name="PessoaSexo")
+    private Pessoa pessoa;
 
     public Pessoa() {
     }
